e6a0ba9b7df37f11db6bd23e7567fb1befb309ab
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-21 20:52:46 -0300

    adding event when submitting new student

--- src/main/java/com/wagnerrmorais/school/EnrollStudentByCommandLine.java
+++ src/main/java/com/wagnerrmorais/school/EnrollStudentByCommandLine.java
@@ -2,6 +2,8 @@ package com.wagnerrmorais.school;
 
 import com.wagnerrmorais.school.academic.application.student.enroll.EnrollStudent;
 import com.wagnerrmorais.school.academic.application.student.enroll.EnrollStudentDTO;
+import com.wagnerrmorais.school.gamification.application.GenerateNoviceStudentBadge;
+import com.wagnerrmorais.school.gamification.infra.badge.BadgeRepositoryInMemory;
 import com.wagnerrmorais.school.shared.domain.event.EventPublisher;
 import com.wagnerrmorais.school.academic.domain.student.StudentEnrolledLogger;
 import com.wagnerrmorais.school.academic.infra.student.repository.StudentInMemoryRepository;
@@ -14,9 +16,13 @@ public class EnrollStudentByCommandLine {
         String cpf = "123.456.789-00";
         String email = "name@someemail.com";
 
-
         EventPublisher publisher = new EventPublisher();
         publisher.addListener(new StudentEnrolledLogger());
+        publisher.addListener(
+                new GenerateNoviceStudentBadge(
+                        new BadgeRepositoryInMemory()
+                )
+        );
 
         EnrollStudent enrollStudent = new EnrollStudent(new StudentInMemoryRepository(), publisher);
 
